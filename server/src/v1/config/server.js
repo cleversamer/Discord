5d978bf28996a6c4bfd32db1e3d0cbfd9a297724
Samer A <thedev.samer@gmail.com>
2022-11-12 20:21:56 +0200

    finished building authentication system on the server

--- server/src/v1/config/server.js
+++ server/src/v1/config/server.js
@@ -1,6 +1,6 @@
 const PORT = process.env["PORT"] || 4000;
 
-const DATABASE_NAME = "demo";
+const DATABASE_NAME = "discord";
 
 const DATABASE_URI =
   process.env["MONGODB_URI"] || `mongodb://127.0.0.1:27017/${DATABASE_NAME}`;
